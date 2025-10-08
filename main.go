1285d52ce1c68397f61a612e8e87780480fef779
Yajanth <yajanthrr@gmail.com>
2025-10-08 22:41:59 +0100

    feat: add structured JSON responses and logging
    - Added unified JSON response format for all endpoints
    - Integrated slog for structured logging
    - Improved shutdown and startup logs

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,10 +19,11 @@ func main() {
 
 	// Routes
 	mux.HandleFunc("/root", handleRoot)
-	mux.HandleFunc("/health", healthCheckFunc)
+	mux.HandleFunc("/health/check", healthCheckFunc)
 	mux.HandleFunc("/health/alive", IsReady)
 
-	fmt.Println("ServeGo is starting.....")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("ServeGo is Starting", "Port", "8082")
 
 	server := &http.Server{
 		Addr:              ":8082",
@@ -32,19 +35,21 @@ func main() {
 	}
 
 	go func() {
-		log.Printf("ServeGo is listening on %s ", server.Addr)
+		logger.Info("ServeGo is Running", "Port", server.Addr)
 		// If the server stopped normally, don’t panic.
 		// But if it failed unexpectedly, log the reason and exit.
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Listen Error: %v", err)
+			logger.Error("Listen Error", "Error", err)
 		}
 	}()
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	<-quit
+	sig := <-quit
+	logger.Warn("Shutdown Signal Recieved", "Signal", sig.String())
 
-	log.Println("Shutting down ServeGO...")
+	fmt.Printf("\n")
+	logger.Info("Shutting down ServeGO")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -57,18 +62,40 @@ func main() {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to ServeGO...")
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := Response{
+		Status:  "success",
+		Message: "Welcome to ServeGo",
+		Time:    time.Now().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(resp)
+
 }
 
 func healthCheckFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Instance is Healthy...")
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := Response{
+		Status:  "Success",
+		Message: "Instance to Healthy",
+		Time:    time.Now().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(resp)
 }
 
 func IsReady(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Instance is ready to use")
+	w.Header().Set("Content-Type", "application/json")
+	resp := Response{
+		Status:  "Success",
+		Message: "Instance is Alive",
+		Time:    time.Now().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(resp)
 }
 
-type Post struct {
-	ID   int    `json:"id"`
-	body string `json:"body"`
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Time    string `json:"time"`
 }
