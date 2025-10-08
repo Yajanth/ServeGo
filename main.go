a74282c6617099787610c4d1d72d1761104cc86c
Yajanth <yajanthrr@gmail.com>
2025-10-08 21:55:49 +0100

    Feat : Added sensible timeout defaults to improve server robustness and prevent resource exhaustion: - ReadHeaderTimeout: 5s - ReadTimeout: 15s - WriteTimeout: 15s - IdleTimeout: 60s
    These defaults help mitigate slowloris attacks and ensure timely connection cleanup.

--- main.go
+++ main.go
@@ -15,17 +15,24 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// Routes
 	mux.HandleFunc("/root", handleRoot)
 	mux.HandleFunc("/health", healthCheckFunc)
+	mux.HandleFunc("/health/alive", IsReady)
 
 	fmt.Println("ServeGo is starting.....")
 
 	server := &http.Server{
-		Addr:    ":8082",
-		Handler: mux}
+		Addr:              ":8082",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       15 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
 
 	go func() {
-		log.Printf("ServeGo is listening on %s", server.Addr)
+		log.Printf("ServeGo is listening on %s ", server.Addr)
 		// If the server stopped normally, don’t panic.
 		// But if it failed unexpectedly, log the reason and exit.
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -39,17 +46,29 @@ func main() {
 
 	log.Println("Shutting down ServeGO...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	server.Shutdown(ctx)
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Graceful shutdown Failed: %v", err)
+	}
+	log.Printf("Server Stopped")
 
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is root HandlerFunction")
+	fmt.Fprintf(w, "Welcome to ServeGO...")
 }
 
 func healthCheckFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Instance is Healthy...")
+}
+
+func IsReady(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Instance is ready to use")
+}
+
+type Post struct {
+	ID   int    `json:"id"`
+	body string `json:"body"`
 }
