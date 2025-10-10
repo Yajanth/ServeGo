75999528484873b808b1e9a1db6b37580b7bdf8b
Yajanth <yajanthrr@gmail.com>
2025-10-10 22:04:42 +0100

    feat: add atomic health conditions and writeJson helper to improve DRY principle
    - Introduced atomic.Bool variables (alive, ready) for thread-safe health state management
    - Created reusable writeJson() function for consistent JSON response formatting
    - Refactored health and root handlers to use writeJson() to eliminate repetition
    - Improved code readability and maintainability

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Yajanth/ServeGo.git
 
 go 1.22.0
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -3,24 +3,41 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 	"time"
+
+	"github.com/google/uuid"
+)
+
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	TraceId string `json:"traceId"`
+	Path    string `json:"path"`
+	TS      string `json:"time"`
+}
+
+var (
+	alive atomic.Bool
+	ready atomic.Bool
 )
 
 func main() {
 
 	mux := http.NewServeMux()
+	alive.Store(true)
+	ready.Store(false)
 
 	// Routes
 	mux.HandleFunc("/root", handleRoot)
-	mux.HandleFunc("/health/check", healthCheckFunc)
-	mux.HandleFunc("/health/alive", IsReady)
+	mux.HandleFunc("/health/check", handleCheck)
+	mux.HandleFunc("/health/ready", handleReady)
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("ServeGo is Starting", "Port", "8082")
@@ -46,9 +63,12 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 	sig := <-quit
+
 	logger.Warn("Shutdown Signal Recieved", "Signal", sig.String())
 
-	fmt.Printf("\n")
+	alive.Store(false)
+	ready.Store(false)
+
 	logger.Info("Shutting down ServeGO")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -57,45 +77,41 @@ func main() {
 	if err := server.Shutdown(ctx); err != nil {
 		log.Printf("Graceful shutdown Failed: %v", err)
 	}
-	log.Printf("Server Stopped")
+	logger.Warn("Shutdown completed gracefully")
 
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	resp := Response{
-		Status:  "success",
-		Message: "Welcome to ServeGo",
-		Time:    time.Now().Format(time.RFC3339),
-	}
-	json.NewEncoder(w).Encode(resp)
+	writeJson(w, r, http.StatusOK, "Welcome to ServeGo", "Success")
 
 }
 
-func healthCheckFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	resp := Response{
-		Status:  "Success",
-		Message: "Instance to Healthy",
-		Time:    time.Now().Format(time.RFC3339),
+func handleCheck(w http.ResponseWriter, r *http.Request) {
+	if alive.Load() {
+		writeJson(w, r, http.StatusOK, "Instance is Alive", "Success")
+		return
 	}
-	json.NewEncoder(w).Encode(resp)
+	writeJson(w, r, http.StatusServiceUnavailable, "Instance is NOT Alive", "Fail")
 }
 
-func IsReady(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	resp := Response{
-		Status:  "Success",
-		Message: "Instance is Alive",
-		Time:    time.Now().Format(time.RFC3339),
+func handleReady(w http.ResponseWriter, r *http.Request) {
+	if ready.Load() {
+		writeJson(w, r, http.StatusOK, "Instance is Ready", "Success")
+		return
 	}
-	json.NewEncoder(w).Encode(resp)
+	writeJson(w, r, http.StatusServiceUnavailable, "Instance is NOT Ready", "Fail")
 }
 
-type Response struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
-	Time    string `json:"time"`
+func writeJson(w http.ResponseWriter, r *http.Request, code int, message string, status string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	resp := Response{
+		Status:  status,
+		Message: message,
+		TraceId: uuid.NewString(),
+		Path:    r.URL.Path,
+		TS:      time.Now().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(resp)
 }
